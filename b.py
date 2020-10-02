b55f6a3d2d95963e23d82f56854add21146afb36
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2020-10-02 20:25:51 +0200

    Discussed things with another dev

--- b.py
+++ b.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass, field
 
+
 @dataclass
 class B:
     description: str
-    value_y: bool
+    value_y: str
     id_b: int = field(default=None)
-  
--- main.py
+++ main.py
@@ -4,39 +4,38 @@ from sqlalchemy.schema import MetaData
 from a import A
 from b import B
 
-metadata = MetaData()
+if __name__ == '__main__':
+    metadata = MetaData()
 
-a_table = sa.Table(
-    'a', metadata,
-    sa.Column('description', sa.String(30), primary_key=True), # I think this is important
-    sa.Column('value_x', sa.Boolean()),
-)
+    a_table = sa.Table(
+        'a', metadata,
+        sa.Column('description', sa.String(30), primary_key=True),  # I think this is important
+        sa.Column('value_x', sa.Boolean()),
+    )
 
-b_table = sa.Table(
-    'b', metadata,
-    sa.Column('id_b', sa.BigInteger, primary_key=True, autoincrement=True),
-    sa.Column('description', sa.String(30), sa.ForeignKey(a_table.c.description), nullable=False),
-    sa.Column('value_y', sa.String(20), nullable=True),    
-)
+    b_table = sa.Table(
+        'b', metadata,
+        sa.Column('id_b', sa.BigInteger, primary_key=True, autoincrement=True),
+        sa.Column('description', sa.String(30), sa.ForeignKey('a.description'), nullable=False),
+        sa.Column('value_y', sa.String(20), nullable=True),
+    )
 
-mapper(A, a_table)
-mapper(B, b_table, properties={
-    'rel': relationship(
-        A, order_by=(a_table.c.description)
-    ),
-})
+    mapper(A, a_table)
+    mapper(B, b_table, properties={
+        'rel': relationship(
+            A, order_by=(a_table.c.description)
+        ),
+    })
 
+    engine = sa.create_engine('sqlite:///x.db', echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    metadata.create_all(engine)
 
-engine = sa.create_engine('sqlite:///x.db', echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-metadata.create_all(engine)
+    a = A(description='DESCRIZIONE', value_x=True)
+    b = B(id_b=3481, description='OPRS', value_y='ASDF')
+    # b.rel = a
 
-
-a = A(description='DESCRIZIONE',value_x=True)
-b = B(description='DESCRIZIONE',value_y='ASDF')
-b.ref = a
-
-session.add(b)
-session.commit()
-
+    session.add(b)
+    # session.add(a)
+    session.commit()
